d406252f7fa1b84139440b2f9254b550c044f43b
helenaskagerlid <helenaskagerlid@gmail.com>
2024-03-28 10:33:30 +0100

    :sparkles: add animation for h1

--- my-vite-project/main.js
+++ my-vite-project/main.js
@@ -1,24 +1,11 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const rainbowEffect = document.querySelector('.rainbow-effect');
 
-setupCounter(document.querySelector('#counter'))
+rainbowEffect.addEventListener('mouseenter', () => {
+  rainbowEffect.style.animation = 'rainbow-animation 10s infinite';
+});
+
+rainbowEffect.addEventListener('mouseleave', () => {
+  rainbowEffect.style.animation = 'none';
+});
